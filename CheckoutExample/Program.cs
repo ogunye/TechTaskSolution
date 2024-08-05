bb8b3ccce81f5d1ca8ff6a61edd78cae19d6d601
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2024-08-05 05:17:35 +0100

    Checkout Console app

---
+++ CheckoutExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using CheckoutTask;
+
+
+//Define unit prices
+var unitPrices = new Dictionary<string, int>
+            {
+                { "A", 50 },
+                { "B", 30 },
+                { "C", 20 },
+                { "D", 15 }
+            };
+
+// Define special prices
+var specialPrices = new Dictionary<string, (int quantity, int specialPrice)>
+            {
+                { "A", (3, 130) },
+                { "B", (2, 45) }
+            };
+
+// Create an instance of Checkout with dynamic pricing rules
+var checkout = new Checkout(unitPrices, specialPrices);
+
+// Scan items
+checkout.Scan("A");
+checkout.Scan("A");
+checkout.Scan("A");
+checkout.Scan("B");
+checkout.Scan("B");
+checkout.Scan("C");
+
+// Get total price
+int totalPrice = checkout.GetTotalPrice();
+Console.WriteLine($"Total Price: {totalPrice}"); // Output: Total Price: 195
+        
